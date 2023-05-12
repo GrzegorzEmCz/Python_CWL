c9d0d3f4dfc9d2d35904b65e4ce00b678df3c5b8
Grzesiek <59139408+GrzegorzEmCz@users.noreply.github.com>
2023-05-12 21:19:13 +0200

    Add zadanie 2

---
+++ Zadanie 2/CzerneckiG_zad2.py
@@ -0,0 +1,44 @@
+from collections import Counter
+
+def count_words(file_path, n):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    words = content.split()
+    word_counts = Counter(words)
+
+    most_common = word_counts.most_common(n)
+    if n < len(word_counts):
+        count_of_nth_word = most_common[-1][1]
+        i = n
+        while i < len(word_counts) and word_counts.most_common(i + 1)[-1][1] == count_of_nth_word:
+            most_common.append(word_counts.most_common(i + 1)[-1])
+            i += 1
+
+    for word, count in most_common:
+        print(word, count)
+
+
+def count_ngrams(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    words = content.split()
+
+    bigrams = [(words[i], words[i+1]) for i in range(len(words)-1)]
+    trigrams = [(words[i], words[i+1], words[i+2]) for i in range(len(words)-2)]
+
+    bigram_counts = Counter(bigrams)
+    trigram_counts = Counter(trigrams)
+
+    print("Bigrams:")
+    for bigram, count in bigram_counts.items():
+        print(' '.join(bigram), count)
+
+    print("\nTrigrams:")
+    for trigram, count in trigram_counts.items():
+        print(' '.join(trigram), count)
+
+
+count_words('potop.txt', 10)
+count_ngrams('potop.txt')
